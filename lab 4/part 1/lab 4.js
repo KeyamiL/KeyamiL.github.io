0801131513189429f4c3aa47ff044e5489e57f54
KeyamiL <keyami.luvito@dcmail.ca>
2026-03-24 13:13:21 -0400

    Constants

--- lab 4/part 1/lab 4.js
+++ lab 4/part 1/lab 4.js
@@ -10,27 +10,16 @@ function randomValueFromArray(array) {
 }
 
 // Raw text strings
-const  
-const
-const
-
-// Willy the Goblin
-// Big Daddy
-// Father Christmas
-
-// the soup kitchen
-// Disneyland
-// the White House
-
-// spontaneously combusted
-// melted into a puddle on the sidewalk
-// turned into a slug and slithered away
+const characters = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+const places = ["the soup kitchen", "Disneyland", "the White House"];
+const events = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and slithered away"];
 
 // Partial return random string function
 
 function returnRandomStoryString() {
-  // It was 94 Fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
 
+  storyText = "It was 94 Fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day";
+  
   storyText = storyText.replaceALL();
   storyText = storyText.replaceALL();
   storyText = storyText.replaceALL();
@@ -58,4 +47,5 @@ function generateStory() {
   story.textContent = "";
   story.style.visibility = "visible";
   story.style.textDecoration = 'underline';
+  story.style.
 }
