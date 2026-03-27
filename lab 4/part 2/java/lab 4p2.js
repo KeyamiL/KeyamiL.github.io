79de2dc982d921b73a152627e7b9bf238dc3b5f7
KeyamiL <keyami.luvito@dcmail.ca>
2026-03-27 12:53:29 -0400

    images

--- lab 4/part 2/java/lab 4p2.js
+++ lab 4/part 2/java/lab 4p2.js
@@ -4,3 +4,5 @@ const thumbBar = document.querySelector(".thumb-bar");
 
 const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
+
+const images = [<img src = "pic1.jpg" alt = ''/>, <img src = "pic2.jpg" alt = ''/>, <img src = "pic3.jpg" alt = ''/>, <img src = "pic4.jpg" alt = ''/>, <img src = "pic5.jpg" alt = ''/>,]
