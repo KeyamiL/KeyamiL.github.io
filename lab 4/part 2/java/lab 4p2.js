13344764b57622536eb8e3d313614d6db688f846
KeyamiL <keyami.luvito@dcmail.ca>
2026-03-30 16:40:22 -0400

    alt texts

--- lab 4/part 2/java/lab 4p2.js
+++ lab 4/part 2/java/lab 4p2.js
@@ -5,4 +5,10 @@ const thumbBar = document.querySelector(".thumb-bar");
 const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
 
-const images = [<img src = "pic1.jpg" alt = ''/>, <img src = "pic2.jpg" alt = ''/>, <img src = "pic3.jpg" alt = ''/>, <img src = "pic4.jpg" alt = ''/>, <img src = "pic5.jpg" alt = ''/>,]
+const images = [<img src = "pic1.jpg" alt = "Closeup of a human eye"/>, <img src = "pic2.jpg" alt = "Rock that looks like a wave"/>,
+                <img src = "pic3.jpg" alt = "Purple and white pansies"/>, <img src = "pic4.jpg" alt = "Section of wall from pharaoh's tomb"/>, 
+                <img src = "pic5.jpg" alt = "Large moth on a leaf"/>];
+
+const baseURL = <a href = "https://mdn.github.io/shared-assets/images/examples/learn/gallery"/>;
+
+<img src ="https://mdn.github.io/shared-assets/images/examples/learn/gallery/pic1.jpg" alt =''/>;          
