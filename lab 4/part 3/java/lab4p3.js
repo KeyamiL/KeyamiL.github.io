d0f8b2cf05dd875494ffde89d7fc7f8ea09fc7ce
KeyamiL <keyami.luvito@dcmail.ca>
2026-03-31 14:46:28 -0400

    collision detection

--- lab 4/part 3/java/lab4p3.js
+++ lab 4/part 3/java/lab4p3.js
@@ -54,11 +54,25 @@ class Ball {
     this.x += this.velX;
     this.y += this.velY;
   }
+  collisionDetect() {
+    for (const ball of balls) {
+      if (this !== ball) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+        }
+      }
+    }
+  }
 }
+
 
 const balls = [];
 
-while (balls.length < 25) {
+while (balls.length < 40) {
   const size = random(10, 20);
   const ball = new Ball(
     // ball position always drawn at least one ball width
@@ -89,6 +103,19 @@ const testBall = new Ball(50, 100, 4, 4, "blue", 10);
     ball.draw();
     ball.update();
   }
+  
+  function loop() {
+  ctx.fillStyle = "rgb(0 0 0 / 25%)";
+  ctx.fillRect(0, 0, width, height);
+
+  for (const ball of balls) {
+    ball.draw();
+    ball.update();
+    ball.collisionDetect();
+  }
+
+  requestAnimationFrame(loop);
+}
 
   requestAnimationFrame(loop);
 }
