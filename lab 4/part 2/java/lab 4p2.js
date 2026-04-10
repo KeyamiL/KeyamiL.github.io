9b7dee6020d4ab0055ccc875f12cfab341f26833
KeyamiL <keyami.luvito@dcmail.ca>
2026-04-09 20:15:55 -0400

    displayImage

--- lab 4/part 2/java/lab 4p2.js
+++ lab 4/part 2/java/lab 4p2.js
@@ -5,17 +5,33 @@ const thumbBar = document.querySelector(".thumb-bar");
 const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
 
+// Image arrays
 const images = [
-               {filename = "pic1.jpg", alt = "Closeup of a human eye"}, {filename = "pic2.jpg", alt = "Rock that looks like a wave"},
-               {filename = "pic3.jpg", alt = "Purple and white pansies"}, {filename = "pic4.jpg", alt = "Section of wall from pharaoh's tomb"}, 
-               {filename = "pic5.jpg", alt = "Large moth on a leaf"}
+               {filename: "pic1.jpg", alt: "Closeup of a human eye"}, {filename: "pic2.jpg", alt: "Rock that looks like a wave"},
+               {filename: "pic3.jpg", alt: "Purple and white pansies"}, {filename: "pic4.jpg", alt: "Section of wall from pharaoh's tomb"}, 
+               {filename: "pic5.jpg", alt: "Large moth on a leaf"}
 ];
 
 const baseURL = "https://mdn.github.io/shared-assets/images/examples/learn/gallery"
 
-for (const image of images)
-    
+//For loop
+for (const image of images) {
+        console.log(image); 
+}           
 
-               
+// Image elements
 
-<img src ="https://mdn.github.io/shared-assets/images/examples/learn/gallery/pic1.jpg" alt = "Closeup of a human eye"/>;          
+const images2 = [
+                { src: "https://mdn.github.io/shared-assets/images/examples/learn/gallery/pic1.jpg", alt: "Closeup of a human eye", },  
+                { src: "https://mdn.github.io/shared-assets/images/examples/learn/gallery/pic2.jpg", alt: "Rock that looks like a wave"},
+ 
+          thumbBar.appendChild(images2),   
+          btn.updateDisplayedImage("click", () => {
+                const displayedImage = '${src} ${alt}';
+ 
+        }
+        )
+          
+]  
+
+
